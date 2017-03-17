f2abb2d82f4eb4cb366bae464ecc0664df92b62f
Fernando Ghisi <fernandoghisi@gmail.com>
2017-03-16 22:32:21 -0300

    CRUD operations for the recipes (using data driven approach for Angular form controlling)

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -6,11 +6,15 @@ import { Ingredient } from "../shared/ingredient";
 @Injectable()
 export class RecipeService {
   private recipes: Recipe[] = [
-    new Recipe('Schnitzel', 'Very tasty', 'http://images.derberater.de/files/imagecache/456xXXX_berater/berater/slides/WienerSchnitzel.jpg', [
+    new Recipe('Schnitzel', 'Very tasty', 'http://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-7_edited.jpg', [
       new Ingredient('French Fries', 2),
       new Ingredient('Pork Meat', 1)
     ]),
-    new Recipe('Summer Salad', 'Okayish', 'http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg', [])
+    new Recipe('Summer Salad', 'Okayish', 'http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg', [
+      new Ingredient('Tomato', 1),
+      new Ingredient('Lettuce', 2),
+      new Ingredient('Arugula', 1),
+    ])
   ];
 
   constructor() {}
@@ -25,6 +29,14 @@ export class RecipeService {
 
   deleteRecipe(recipe: Recipe) {
     this.recipes.splice(this.recipes.indexOf(recipe), 1)
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+  }
+
+  editRecipe(oldRecipe, newRecipe) {
+    this.recipes[this.recipes.indexOf(oldRecipe)] = newRecipe;
   }
 
 }
